45880f59df761171d29da58731d3a3fa65811ee9
tanahiro2010 <herentongkegu087@gmail.com>
2025-04-13 11:35:46 +0900

    Edit main.py to render deploy

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
         log("  Auth failed. please change token.")
         return
 
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 if __name__ == '__main__':
     main()
